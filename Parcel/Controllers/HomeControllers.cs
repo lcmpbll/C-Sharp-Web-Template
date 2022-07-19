f13d81ca59cd06bbd70197409fe9464a17e621e0
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-19 16:10:33 -0700

    WIP time for white boarding.

--- Parcel/Controllers/HomeControllers.cs
+++ Parcel/Controllers/HomeControllers.cs
@@ -5,16 +5,27 @@ namespace Parcel.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/hello")]
-    public string Hello() {return "Hello friend!"; }
-
-    [Route("/")]
-    public ActionResult Project() 
+    [HttpPost("/index")]
+    public ActionResult Create(int height, int width, int length, int weight) 
     {
-      ProjectVariable myProjectVariable = new ProjectVariable();
-      myProjectVariable.Verb = "Runs";
-      return View(myProjectVariable); 
+      ParcelVariable newParcel = new ParcelVariable(height, width, length, weight);
+      newParcel.Height = height;
+      return RedirectToAction("Index"); 
     }
+
+    [HttpGet("/index")]
+    public ActionResult Index()
+    {
+      return View("index");
+    }
+
+    // [Route("/")]
+    // public ActionResult Project() 
+    // {
+    //   // ProjectVariable myProjectVariable = new ProjectVariable();
+    //   // myProjectVariable.Verb = "Runs";
+    //   // return View(myProjectVariable); 
+    // }
 
   }
 }
